d56483560a6de2d5c40c10b57c056df074f8fff8
Leon <84141927+eyelexx@users.noreply.github.com>
2023-05-18 15:17:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function() {
+    const novaTarefa = $('<li></li>');
+    
+    $('form').on('submit', function(evento) {
+        evento.preventDefault();
+
+        const inputTarefa = $('.tarefa').val();
+
+        $(`${inputTarefa}`).appendTo(novaTarefa); // Insere o valor digitado no inputo dentro dos <li> da variável NovaTarefa
+        
+        $(`<li>
+            ${inputTarefa}
+        </li>`).appendTo('ul');
+        
+        $('.tarefa').val(''); // Reseta o input de tarefa
+    })
+
+    $('ul').on('click', 'li', function() { // Mudança de Tarefa Concluída/Não Concluída
+        $(this).toggleClass('concluido');
+    });
+
+    $('.limpar').on('click', () => { // Limpa a lista
+        console.log('ok');
+        $('ul').html('')
+    })
+
+})
