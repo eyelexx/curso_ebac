71b7d0f607d38e9e69964845cf4ca2fa7832607f
Leon <84141927+eyelexx@users.noreply.github.com>
2023-05-31 21:43:20 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function() {
+    
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+    
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome.'
+        },
+    })
+
+    $('form').on('submit', function(evento) {
+        evento.preventDefault();
+
+        const inputs = $('input');
+        const resultado = $('.result');
+
+        if(!inputs.hasClass('error')) {
+            resultado.html("Cadastro validado com sucesso!");
+        } else {
+            resultado.css("display", "none")
+        }
+    })
+})
+
