7ae16ba9eba2c5beb9ed94be82a1d819b547e7f6
Douglas Eduardo <douglaspatusco@gmail.com>
2023-08-14 11:08:08 -0300

    Exercício - Módulo 23

---
+++ main.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const avatar = document.querySelector('#avatar');
+    const name = document.querySelector('#name');
+    const user = document.querySelector('#username');
+    const repos = document.querySelector('#repos');
+    const followers = document.querySelector('#followers');
+    const following = document.querySelector('#following');
+    const link = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/eyelexx')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            avatar.src = json.avatar_url;
+            name.innerText = json.name;
+            user.innerText = json.login;
+            repos.innerText = json.public_repos;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+            link.href = json.html_url;
+        })
+})
