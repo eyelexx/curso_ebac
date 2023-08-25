282cda38bac020d9b75d16adfa05b7b308a4f1e9
Douglas Eduardo <douglaspatusco@gmail.com>
2023-08-25 18:47:38 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+function  multiplicacao(a: number, b: number): number {
+    return a * b ;
+}
+
+function olaUsuario(nomeUsuario: string): string {
+    return `Olá, ${nomeUsuario}!`
+}
+
+console.log(multiplicacao(2, 5))
+console.log(olaUsuario('Douglas'))
